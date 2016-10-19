9bcaee24b9e72250cfc8f40aa82896b72d802182
vg <viktorgrom84@gmail.com>
2016-10-19 16:07:32 -0700

    Appoitment

---
+++ Mad-Libs/js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(even) {
+    var person1Input = $("input#person1").val();
+
+    $(".person1").text(person1Input);
+
+
+       $("#story").show();
+
+
+       event.preventDefault();
+
+       var
+
+     });
+     $(".person1").text()
+       $(this).toUpperCase()
+
+       $("#story").text()
+         $(this).toUpperCase()
+
+   });
+   });
